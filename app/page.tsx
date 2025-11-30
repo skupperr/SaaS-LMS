e8f0051e152880ceb25cee57a00ac80189ae60d8
Asif Uddin Ahmed <mylifeasasif@gmail.com>
2025-11-30 23:16:48 +0100

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
+import CompanionCard from "@/components/companionCard"
+import CompanionList from "@/components/companionLists"
+import Cta from "@/components/CTA"
+import { recentSessions } from "@/constants"
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Hello world</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1 className=''>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id='123'
+          name="Neura the brainy explorer"
+          topic="Neural Network of the brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id='124'
+          name="Neura the brainy explorer"
+          topic="Neural Network of the brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id='125'
+          name="Neura the brainy explorer"
+          topic="Neural Network of the brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames='w-2/3 max-lg:w-full' 
+        />
+        <Cta/>
+      </section>
+    </main>
   )
 }
 
