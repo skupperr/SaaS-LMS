de4a326a80e29f252a7ac77a68e2fa3e5e31b328
Asif Uddin Ahmed <mylifeasasif@gmail.com>
2025-12-03 00:48:08 +0100

    companion form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,14 @@
+import CompanionForm from "@/components/companionForm"
 
 const NewCompanion = () => {
     return (
-        <div>Page</div>
+        <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+            <article className="w-full gap-4 flex flex-col">
+                <h1>Companion Builder</h1>
+
+                <CompanionForm/>
+            </article>
+        </main>
     )
 }
 
